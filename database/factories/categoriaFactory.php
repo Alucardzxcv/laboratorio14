52ef232809c20cbd88f794a7a37b76eb0211fb4a
ManuelHuayhua <gilder.huayhua@tecsup.edu.pe>
2022-05-29 13:35:47 -0500

    Lab11

---
+++ database/factories/categoriaFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class categoriaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'descipcion' => $this->faker->text(),
+            'email' => $this->faker->unique()->safeEmail(),
+
+        ];
+    }
+}
